ba918a5868fa01a0d932568c840a760069968f2e
ohmmypokoko <48791680+ohmmypokoko@users.noreply.github.com>
2019-04-04 19:04:31 +0700

    Add files via upload

--- staff.sql
+++ staff.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS `staff`;
 
 CREATE TABLE `staff` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `staff_no` varchar(5) ,
-  `f_name` varchar(100) ,
-  `l_name` varchar(100) ,
-  `position` varchar(50) ,
-  `sex` varchar(2) ,
-  `d_o_b` varchar(10) ,
-  `salary` float(7,2) ,
-  `branch_id` int(11) ,
-  `user_id` int(11)  ,
+  `staff_no` varchar(5) DEFAULT NULL,
+  `f_name` varchar(100) DEFAULT NULL,
+  `l_name` varchar(100) DEFAULT NULL,
+  `position` varchar(50) DEFAULT NULL,
+  `sex` varchar(2) DEFAULT NULL,
+  `d_o_b` date DEFAULT NULL,
+  `salary` float(7,2) DEFAULT NULL,
+  `branch_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
